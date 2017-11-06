1d8b9ad04697f9539ac511c30b384a05bf402bf7
kajah <katharine.jiang9@gmail.com>
2017-11-06 14:42:10 -0800

    added react router

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+/**
+ * Created by kj on 11/6/17.
+ */
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>Home</div>
+        );
+    }
+}
+
+export default Home;
--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
 import logo from '../assets/feitiandancerslogo.jpg';
 import '../styles/css/navbar.css';
+import { Link } from 'react-router-dom'
 
 class Navbar extends Component {
 
     render() {
         return (
             <nav>
-                <div className="logo">
-                    <img src={logo} className="ftd-logo" alt="logo" />
-                </div>
-                <div className="menu">
-                    <a href="/members">Members</a>
-                    <a href="/multimedia">Multimedia</a>
-                </div>
+                <ul>
+                    <div className="logo">
+                        <li><Link to="/"><img src={logo} className="ftd-logo" alt="logo" /></Link></li>
+                    </div>
 
+                    <div className="menu">
+                        <li><Link to="/members">Members</Link></li>
+                        <li><Link to="/media">Multimedia</Link></li>
+                    </div>
+
+                </ul>
             </nav>
 
         );
