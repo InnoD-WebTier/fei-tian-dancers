219b8828e6a179a6c779ac4e7604f859397a1cd0
Ariel Chen <arielchen@berkeley.edu>
2017-11-20 23:55:55 -0800

    photogrid, and added an underline shadow to nav bar

--- src/pages/Members.js
+++ src/pages/Members.js
@@ -1,12 +1,16 @@
-/**
- * Created by kj on 11/6/17.
- */
 import React, { Component } from 'react';
+import PhotoGrid from '../components/PhotoGrid.js';
+import PageHeader from '../components/PageHeader.js';
+
 
 class Members extends Component {
     render() {
+    	const title = 'Our Team';
         return (
-            <div>Members</div>
+            <div>
+            	<PageHeader title={title}></PageHeader>
+            	<PhotoGrid/>
+            </div>
         );
     }
 }
