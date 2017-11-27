5434373211ed738b9ca3bcbd81eb9d1b42d8e7f6
Holly Luo <hollyluo1996@berkeley.edu>
2017-11-27 10:19:08 -0800

    FTD homepage WIP

---
+++ src/pages/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Home from './home';
+
+// A base file for all your pages to come together.
+
+class App extends Component {
+  render() {
+    return (
+      <Home/>
+    );
+  }
+}
+
+export default App;
