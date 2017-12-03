f43cd1683b0c7ed0e7e33a2f8b65c64ace95db00
Peter Qiu <peterqiu1997@gmail.com>
2017-12-03 14:33:48 -0800

    updated home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,9 @@
  */
 import React, { Component } from 'react';
 import cover from '../assets/cover.png';
+import facebook from '../assets/facebook.png';
+import youtube from '../assets/youtube.png';
+import mail from '../assets/mail.png';
 import PageHeader from '../components/PageHeader';
 
 class Home extends Component {
@@ -23,9 +26,38 @@ For those with more Chinese dance experience, we hold auditions for the FTD perf
                 </div>
                 <div className="join">
                     <PageHeader title={"Join Us"}></PageHeader>
+                    <div className="join__section">
+                        <div className="join__title">FAQs</div>
+                        <div className="join__subtitle">How much is the club fee?</div>
+                        <div className="join__text">Club fees are $30. These fees go to paying for costumes and banquet.</div>
+
+                        <div className="join__subtitle">What is the time commitment like?</div>
+                        <div className="join__text">Practice times and frequency depend on the choreographer and dancers' schedules. They are typically 3-4 hours a week. We may also offer minipieces, which require less time committment, typically 1-2 hours a week.</div>
+                    </div><br/>
+                    <div className="join__section">
+                        <div className="join__title">AUDITIONS</div>
+                        <div className="join__subtitle">ATTIRE</div>
+                        <div className="join__text">No baggy pants, jeans, or baggy shirts. 
+Bring dance shoes (ballet shoes, jazz shoes, etc.) or your bare feet. </div>
+
+                        <div className="join__subtitle">THE AUDITION ITSELF</div>
+                        <div className="join__text">Warm-ups are done as a group for the first few minutes. We will be teaching you a short dance sequence, then you will have a chance to perform what you learned. We will be looking for: attention to detail, ability to pick up choreography, performance skills, etc. All returning members will be auditioning as well, so everyone will have an equal chance to be in the piece. 
+</div>
+                    </div>
                 </div>
                 <div className="contact">
                     <PageHeader title={"Contact Us"}></PageHeader>
+                    <div className="contact__icons">
+                        <img className="facebook" src={facebook}/>
+                        <img className="youtube" src={youtube}/>
+                        <img className="mail" src={mail}/>
+                    </div>
+                    <div className="contact__text">
+                    Please email us at <span className="email">dance.chinese@gmail.com</span> if you have any questions or inquiries about our group.
+                    <br/>
+                    <br/>
+                    We will reply as soon as we can. Thank you!
+                    </div>
                 </div>
             </div>
         );
